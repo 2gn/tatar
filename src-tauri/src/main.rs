3b09e4291684888b9a195f1940f81589fe61c37c
2gn <101851090+2gn@users.noreply.github.com>
2022-11-13 12:58:26 +0900

    vue can be removed

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -25,7 +25,7 @@ fn main() {
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
             )
             .build()?;
-            app.get_window("main").unwrap().close();
+            // app.get_window("main").unwrap().close();
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![log_to_console])
