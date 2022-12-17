19cac0f56dfb6adac00fdaf8f5030b8e4422e64a
2gn <101851090+2gn@users.noreply.github.com>
2022-12-17 23:14:44 +0900

    worked

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -23,19 +23,6 @@ fn logical_size(width: f64, height: f64) -> Option<Size> {
 fn main() {
     Builder::default()
         .setup(|app| {
-            let window = WindowBuilder::new(
-                app,
-                "label",
-                WindowUrl::External(Url::parse("https://music.youtube.com").unwrap())
-            )
-            .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0")
-            .build()?;
-            window.set_min_size(
-                logical_size(
-                    320.,
-                    320.
-                )
-            )?;
             Ok(())
         })
         .invoke_handler(generate_handler![log_to_console])
