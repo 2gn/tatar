8cb9bf8dcb885baf1f9413c79c1ec7bf1f3e16e0
Hiram Tanner <101851090+2gn@users.noreply.github.com>
2024-03-20 22:33:59 +0900

    replaced icons and updated README

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 <h1 align="center">
-    <img src="./src-tauri/icons/tatar_128x128.png" width=128 height=128/><br>
-    tatar(WIP)
+    <img src="./src-tauri/icons/tatar.png" width=128 height=128/><br>
+    tatar
 </h1>
 
 <h4 align="center">
     A youtube music client built with tauri.
 </h4>
 
-## download
-This project is a work-in-progress. Stability won't be promised.
-You can download pre-release from [release section]("https://github.com/2gn/tatar/releases")
+## Download
+You can download executables from [releases]("https://github.com/2gn/tatar/releases")
 
-## screenshots
+## Contributions
+Feel free to create some issues. I'll try my best to make the situation better.
+
+Pull requests are welcome. Please send some.
+
+## Screenshots
 
 ![screenshot](./screenshots/1.png)
 
 ![screenshot](./screenshots/2.png)
+
+## FAQ
+
+### I cannot quit the app
+On windows, from the system tray, right-click on the tatar icon and select `Quit`
+
+On MacOS, I don't know (I don't have MacOS to test this on, sorry). Open Activity Monitor, find tatar and quit it.
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,54 +1,77 @@
-// use tauri::{
-//     generate_context, generate_handler, Builder, LogicalSize, Size, WindowBuilder, WindowUrl, Menu
-// };
-use std;
-use tauri::*;
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
-// #![cfg_attr(
-//     all(not(debug_assertions), target_os = "windows"),
-//     windows_subsystem = "windows"
-// )]
+use tauri::SystemTray;
+use tauri::*;
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
-#[tauri::command]
-fn log_to_console(msg: &str) {
-    println!("{}", msg);
-}
-
 fn main() {
-    let settings_button_injector_js = "
-    const open_settings = new Event('open-settings');
-    let top_bar = document.getElementsByClassName('center-content style-scope ytmusic-nav-bar')[0];
-    let settings_button = document.createElement('button');
-    settings_button.innerText = 'Settings';
-    settings_button.addEventListener('click', (event) => {;
-        window.dispatchEvent('open-settings');
-    });
-    top_bar.prepend(settings_button);
-    ";
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
+    // let settings_button_injector_js = "
+    // const open_settings = new Event('open-settings');
+    // let top_bar = document.getElementsByClassName('center-content style-scope ytmusic-nav-bar')[0];
+    // let settings_button = document.createElement('button');
+    // settings_button.innerText = 'Settings';
+    // settings_button.addEventListener('click', (event) => {;
+    //     window.dispatchEvent('open-settings');
+    // });
+    // top_bar.prepend(settings_button);
+    // ";
 
     Builder::default()
+        .system_tray(system_tray)
         .setup(|app| {
             #[cfg(debug_assertions)]
             {
                 let main_window = app.get_window("main").unwrap();
-                let handle = app.handle();
+                // let handle = app.handle();
 
-                main_window
-                    .eval(settings_button_injector_js)
-                    .expect("failed to run the script");
+                // main_window
+                //     .eval(&settings_button_injector_js)
+                //     .expect("failed to run the script");
 
-                app.listen_global("open-settings", move |_| {
-                    handle
-                        .get_window("settings")
-                        .unwrap()
-                        .show()
-                        .expect("could not show settings window");
-                });
+                // app.listen_global("open-settings", move |_| {
+                //     handle
+                //         .get_window("settings")
+                //         .unwrap()
+                //         .show()
+                //         .expect("could not show settings window");
+                // });
             }
             Ok(())
         })
+        .on_window_event(|event| match event.event() {
+            WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => app
+                .get_window("main")
+                .unwrap()
+                .show()
+                .expect("failed to show"),
+            _ => {}
+        })
         .run(generate_context!())
-        .expect("failed")
+        .expect("failed");
 }
